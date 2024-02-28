d33dcdfcebaede1366451db0bf404305aa3dcce2
Alex Argyriou <Alex.argyriou@vodafone.com>
2024-02-28 20:39:11 +0200

    wow change

--- src/main/java/com/argyriou/demoactions/DemoActionsApplication.java
+++ src/main/java/com/argyriou/demoactions/DemoActionsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoActionsApplication {
 
+    // wow
     public static void main(String[] args) {
         SpringApplication.run(DemoActionsApplication.class, args);
     }
