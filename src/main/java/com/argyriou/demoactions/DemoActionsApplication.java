ef56503086bdcfd21cff5845ab527513e4aedf3e
Alex Argyriou <Alex.argyriou@vodafone.com>
2024-02-28 20:41:11 +0200

    my wow changeee

--- src/main/java/com/argyriou/demoactions/DemoActionsApplication.java
+++ src/main/java/com/argyriou/demoactions/DemoActionsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoActionsApplication {
 
     public static void main(String[] args) {
+        // my own wow commit
         SpringApplication.run(DemoActionsApplication.class, args);
     }
 
