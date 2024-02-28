2da882546fb424169a0f65052115b39879fff204
Alex Argyriou <Alex.argyriou@vodafone.com>
2024-02-28 20:37:56 +0200

    Initial commit

---
+++ src/test/java/com/argyriou/demoactions/DemoActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argyriou.demoactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
